293be4120221d93cf4f99eb4f187edb5ad48fc21
sandracascantem <sandra.cascantem@alumnos.upm.es>
2023-04-12 17:23:05 +0200

    perfilready

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -74,44 +74,44 @@ class _ProfilePageState extends State<ProfilePage> {
           },
           child: ListView(
             children: [
-              // Center(
-              //   child: Stack(
-              //     children: [
-              //       Container(
-              //         width: 130,
-              //         height: 130,
-              //         decoration: BoxDecoration(
-              //           border: Border.all(width: 4, color: Colors.white),
-              //           boxShadow: [
-              //             BoxShadow(
-              //                 spreadRadius: 2,
-              //                 blurRadius: 10,
-              //                 color: Colors.black.withOpacity(0.1)),
-              //           ],
-              //           shape: BoxShape.circle,
-              //           image: DecorationImage(
-              //               fit: BoxFit.cover,
-              //               image: AssetImage(
-              //                   'assets/usuario_foto.png'))),
-              //       ),
-              //       Positioned(
-              //         bottom: 0,
-              //         right: 0,
-              //         child: Container(
-              //           height: 40,
-              //           width: 40,
-              //           decoration: BoxDecoration(
-              //               shape: BoxShape.circle,
-              //               border:
-              //                   Border.all(width: 4, color: Colors.green),
-              //               color: Colors.white),
-              //           child: Icon(
-              //             Icons.edit,
-              //             color: Colors.green,
-              //           )))
-              //     ],
-              //   ),
-              // ),
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 4, color: Colors.white),
+                        boxShadow: [
+                          BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1)),
+                        ],
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(
+                                'assets/usuario_foto.png'))),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border:
+                                Border.all(width: 4, color: Colors.green),
+                            color: Colors.white),
+                        child: Icon(
+                          Icons.edit,
+                          color: Colors.green,
+                        )))
+                  ],
+                ),
+              ),
               // Center(
               //   child: DefaultTextStyle(
               //     style: TextStyle(
